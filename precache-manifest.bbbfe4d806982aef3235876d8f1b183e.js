6b8575f9174c743ebea1f418091cc4e49410b4ac
debugleader.abdallah <aboudi41444@gmail.com>
2020-10-09 10:37:24 +0400

    Updates

---
+++ precache-manifest.bbbfe4d806982aef3235876d8f1b183e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f408367dda87824c8c0b57b8b62eab0",
+    "url": "/debugleader.github.io/ToDoX/index.html"
+  },
+  {
+    "revision": "3c2f3d4fe00e2b4ddcef",
+    "url": "/debugleader.github.io/ToDoX/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "eaf313566011079ad915",
+    "url": "/debugleader.github.io/ToDoX/static/css/main.d5635a52.chunk.css"
+  },
+  {
+    "revision": "3c2f3d4fe00e2b4ddcef",
+    "url": "/debugleader.github.io/ToDoX/static/js/2.868c63d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/debugleader.github.io/ToDoX/static/js/2.868c63d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf313566011079ad915",
+    "url": "/debugleader.github.io/ToDoX/static/js/main.d3c070c0.chunk.js"
+  },
+  {
+    "revision": "e0ecf75db06825050ba6",
+    "url": "/debugleader.github.io/ToDoX/static/js/runtime-main.d3dd9d3e.js"
+  }
+]);
