c7f3ba741c694cf7193036f726150d47de4b305a
debugleader.abdallah <aboudi41444@gmail.com>
2020-10-09 11:02:49 +0400

    Fixed another small issue

---
+++ build/precache-manifest.9d97e82e4541d7cd3d1ba2054b9bc849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3364c68070345479e5e209b0be417ad1",
+    "url": "/TodoX/index.html"
+  },
+  {
+    "revision": "3c2f3d4fe00e2b4ddcef",
+    "url": "/TodoX/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "4e0275ba88eef9848ead",
+    "url": "/TodoX/static/css/main.d5635a52.chunk.css"
+  },
+  {
+    "revision": "3c2f3d4fe00e2b4ddcef",
+    "url": "/TodoX/static/js/2.868c63d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoX/static/js/2.868c63d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0275ba88eef9848ead",
+    "url": "/TodoX/static/js/main.37feb22f.chunk.js"
+  },
+  {
+    "revision": "85b074ceb5691dd19068",
+    "url": "/TodoX/static/js/runtime-main.0a353494.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/debugleader.github.io/ToDoX/precache-manifest.bbbfe4d806982aef3235876d8f1b183e.js"
+  "/TodoX/precache-manifest.9d97e82e4541d7cd3d1ba2054b9bc849.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/debugleader.github.io/ToDoX/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TodoX/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
